235c99fcec2c8cd534f8574bbfd21c65353f9c59
VISHALKANNAN070 <vishalkannan070@gmail.com>
2025-03-19 14:55:20 +0530

    delete

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,33 @@
-import './styles/App.css'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Header from './pages/Header'
-import Home from './pages/Home'
-import About from './pages/About'
-import Project from './pages/Project'
-import Contact from './pages/Contact'
+import './styles/App.css';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Link } from 'react-scroll';
+import Home from './pages/Home';
+import About from './pages/About';
+import Project from './pages/Project';
+import Contact from './pages/Contact';
+
 function App() {
   return (
-    <>
-     <Header />
-    <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='/about' element={<About />} />
-      <Route path='/projects' element={<Project />} />
-      <Route path='/contact' element={<Contact />} />
-    </Routes>
-    </>
-  )
+    <Router>
+      <header>
+        <nav>
+          <ul>
+            <li><Link to="home" smooth={true} duration={500}>Home</Link></li>
+            <li><Link to="about" smooth={true} duration={500}>About</Link></li>
+            <li><Link to="projects" smooth={true} duration={500}>Projects</Link></li>
+            <li><Link to="contact" smooth={true} duration={500}>Contact</Link></li>
+          </ul>
+        </nav>
+      </header>
+
+      <main>
+        <section id="home"><Home /></section>
+        <section id="about"><About /></section>
+        <section id="projects"><Project /></section>
+        <section id="contact"><Contact /></section>
+      </main>
+    </Router>
+  );
 }
 
-export default App
+export default App;
