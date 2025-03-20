f3af2bd673aea75dc3c2506a382e0f3b4a58327d
vickyy234 <vickymayilsamy@gmail.com>
2025-03-20 23:28:05 +0530

    Updated SpeedInsights and Analytics from vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import './styles/App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 import Header from './pages/Header'
 import Home from './pages/Home'
 import About from './pages/About'
@@ -14,7 +16,9 @@ function App() {
       <Route path='/about' element={<About />} />
       <Route path='/projects' element={<Project />} />
       <Route path='/contact' element={<Contact />} />
-    </Routes>
+      </Routes>
+      <Analytics/>
+      <SpeedInsights/>
     </>
   )
 }
