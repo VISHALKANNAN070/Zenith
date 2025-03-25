f89d61791cfdb5e39200089b5c4a6face014535b
vickyy234 <vickymayilsamy@gmail.com>
2025-03-25 16:21:08 +0530

    Updated Heaer.jsx

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -1,19 +1,18 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 import '../styles/header.css'
 import logo from '/public/Tz_White-removebg.png'
 function Header() {
   return (
     <header className='header'>
       <img src={logo} alt="Zenith Logo" />
-       <nav className='navbar'>
+      <nav className='navbar'>
         <ul>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/About">About</Link></li>
-            <li><Link to="/Projects">Projects</Link></li>
-            <li><Link to="/Contact">Contact</Link></li>
+          <li><a href="#home">Home</a></li>
+          <li><a href="#about">About</a></li>
+          <li><a href="#project">Projects</a></li>
+          <li><a href="#contact">Contact</a></li>
         </ul>
-       </nav>
+      </nav>
     </header>
   )
 }
